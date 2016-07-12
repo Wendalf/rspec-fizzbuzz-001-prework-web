cde920a52ef24bcf20a1c8dc07e9b7a72e55c524
Guangwen Xu <guangw.xu@gmail.com>
2016-07-12 17:46:45 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,9 @@
-def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
+def fizzbuzz(n)
+  if n % 3 == 0 && n % 5 == 0
     "FizzBuzz"
-  elsif number % 3 == 0
+  elsif n % 3 == 0
     "Fizz"
-  elsif number % 5 == 0
+  elsif n % 5 == 0
     "Buzz"
   end
 end
-    
